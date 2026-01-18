483596e6b1f38789d00571f7592d80a18f1c776c
Kushagra Bainsla <kushagrabainsla@gmail.com>
2026-01-17 20:21:40 -0800

    Hotfix: Updated workflow files and init code for the services

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -92,10 +92,8 @@ resource "aws_ecs_service" "backend" {
     assign_public_ip = true
   }
 
-  deployment_configuration {
-    maximum_percent         = 200
-    minimum_healthy_percent = 100
-  }
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
   lifecycle {
     ignore_changes = [task_definition, desired_count]
